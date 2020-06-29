c13c7f65408150c9d4e692edca70badb598112f2
inman <inman@gitea.qwlocal>
2020-06-28 23:33:13 -0400

    remove password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -5,5 +5,5 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 # Comment out this line or first hash your own password
-c.NotebookApp.password = u'sha1:6f5a363f229c:2dc70cdb2b05b0ef003c4c4d911f3bd6a661fa87'
+#c.NotebookApp.password = u'sha1:6f5a363f229c:2dc70cdb2b05b0ef003c4c4d911f3bd6a661fa87'
 c.ContentsManager.allow_hidden = True
